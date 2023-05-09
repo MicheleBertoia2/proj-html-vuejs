956a82f4a3a85dd96760203d34ae9125ad6e46ba
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-05-09 17:25:01 +0200

    [add] db for header menu

---
+++ src/data/db-menus.js
@@ -0,0 +1,292 @@
+export {headerMenu}
+
+const headerMenu = [
+  {
+    title: "Home",
+    href: "#",
+    sublinks:  [
+      {
+        title: "MaxCoach Education",
+        href: "#",
+        isHot: true,
+        isNew: false
+      },
+      {
+        title: "Course Portal",
+        href: "#",
+        isHot: false,
+        isNew: false
+      },
+      {
+        title: "Distant Learning",
+        href: "#",
+        isHot: true,
+        isNew: false
+      },
+      {
+        title: "Multimedia Pedagogy",
+        href: "#",
+        isHot: false,
+        isNew: false
+      },
+      {
+        title: "Modern Schooling",
+        href: "#",
+        isHot: false,
+        isNew: false
+      },
+      {
+        title: "Remote Training",
+        href: "#",
+        isHot: false,
+        isNew: false
+      },
+      {
+        title: "Health Coaching",
+        href: "#",
+        isHot: false,
+        isNew: false
+      },
+      {
+        title: "Gym Coaching",
+        href: "#",
+        isHot: false,
+        isNew: false
+      },
+      {
+        title: "Business",
+        href: "#",
+        isHot: false,
+        isNew: false
+      },
+      {
+        title: "Artist",
+        href: "#",
+        isHot: false,
+        isNew: false
+      },
+      {
+        title: "Kitchen Coach",
+        href: "#",
+        isHot: false,
+        isNew: false
+      },
+      {
+        title: "Motivation",
+        href: "#",
+        isHot: false,
+        isNew: false
+      },
+      {
+        title: "Dancing",
+        href: "#",
+        isHot: false,
+        isNew: false
+      },
+      {
+        title: "Guitar",
+        href: "#",
+        isHot: false,
+        isNew: false
+      },
+      {
+        title: "Yoga",
+        href: "#",
+        isHot: false,
+        isNew: false
+      },
+      {
+        title: "Photography",
+        href: "#",
+        isHot: false,
+        isNew: false
+      },
+      {
+        title: "Personal Finance",
+        href: "#",
+        isHot: false,
+        isNew: false
+      },
+      {
+        title: "Sales Coaching",
+        href: "#",
+        isHot: false,
+        isNew: true
+      },
+      {
+        title: "Mental Therapy",
+        href: "#",
+        isHot: false,
+        isNew: true
+      },
+    ]
+  },
+  {
+    title: "Pages",
+    href: "#",
+    sublinks:[
+      {
+        title: "Start Here",
+        href: "#"
+      },
+      {
+        title: "Succes Story",
+        href: "#"
+      },
+      {
+        title: "About Me",
+        href: "#"
+      },
+      {
+        title: "About Us 01",
+        href: "#"
+      },
+      {
+        title: "About Us 02",
+        href: "#"
+      },
+      {
+        title: "About Us 03",
+        href: "#"
+      },
+      {
+        title: "Contact Me",
+        href: "#"
+      },
+      {
+        title: "Contact Us",
+        href: "#"
+      },
+      {
+        title: "Purchase Guide",
+        href: "#"
+      },
+      {
+        title: "Privacy Policy",
+        href: "#"
+      },
+      {
+        title: "Terms Of Service",
+        href: "#"
+      },
+    ]
+  },
+  {
+    title: "Courses",
+    href: "#",
+    sublinks:[
+      {
+        title: "Courses Grid 01",
+        href: "#"
+      },
+      {
+        title: "Courses Grid 02",
+        href: "#"
+      },
+      {
+        title: "Courses Grid 03",
+        href: "#"
+      },
+      {
+        title: "Membership Levels",
+        href: "#"
+      },
+      {
+        title: "Become a Teacher",
+        href: "#"
+      },
+      {
+        title: "Profile",
+        href: "#"
+      },
+      {
+        title: "Checkout",
+        href: "#"
+      },
+      {
+        title: "Single Layout",
+        href: "#",
+        subsublinks: [
+          {
+            title:"Free Course",
+            href: "#"
+          },
+          {
+            title:"Sticky Features Bar",
+            href: "#"
+          },
+          {
+            title:"Standard Sidebar",
+            href: "#"
+          },
+          {
+            title:"No Sidebar",
+            href: "#"
+          },
+        ]
+      },
+    ]
+  },
+  {
+    title: "Features",
+    href: "#",
+    sublinks:[
+      {
+        title: "Events",
+        href: "#",
+      },
+      {
+        title: "Zoom Meetings",
+        href: "#",
+      },
+    ]
+  },
+  {
+    title: "Blog",
+    href: "#",
+    sublinks:[
+      {
+        title: "Blog Grid",
+        href: "#",
+      },
+      {
+        title: "Blog Masonry",
+        href: "#",
+      },
+      {
+        title: "Blog Classic",
+        href: "#",
+      },
+      {
+        title: "Blog List",
+        href: "#",
+      },
+    ]
+  },
+  {
+    title: "Shop",
+    href: "#",
+    sublinks:[
+      {
+        title: "Shop Left Sidebar",
+        href: "#",
+      },
+      {
+        title: "Shop Right Sidebar",
+        href: "#",
+      },
+      {
+        title: "Cart",
+        href: "#",
+      },
+      {
+        title: "Wishlist",
+        href: "#",
+      },
+      {
+        title: "Single Product",
+        href: "#",
+      },
+    ]
+  }
+]
